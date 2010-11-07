3088acc41f60a27b9c2f854271c2f749a806a3fe
Adrian Smith <adrian@17od.com>
2010-11-07 17:23:49 +0000

    Added the Google Analytics code and a few other small fixups

--- header.php
+++ header.php
@@ -1,14 +1,21 @@
-<?xml version="1.0" encoding="UTF-8"?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<!DOCTYPE HTML>
+<html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title>Adrian Smith's Blog</title>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>"/>
     <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="http://feeds.feedburner.com/AdrianSmithsBlog" />
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-1107504-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </head>
   <body>
 
