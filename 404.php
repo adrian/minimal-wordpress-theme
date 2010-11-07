f41f8bf3ab656109dfa9335a86c46edcaac92985
Adrian Smith <adrian@17od.com>
2010-11-07 17:46:43 +0000

    Added 404.html

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<p>
+  The page you're looking for doesn't appear to exist.
+</p>
+<p>
+  Take a look at the <a href="<?php bloginfo( 'url' ); ?>">index page</a> to see if you can find what you're after.
+</p>
+<?php get_footer(); ?>
